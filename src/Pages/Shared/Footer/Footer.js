9110c8ff9231eeffb08ce7a845bd51c6913077dc
ariful892 <ariful00892@gmail.com>
2022-04-17 01:27:57 +0600

    project needed component created and react framework and library initiated

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className='text-center mt-5'>
+            <p>
+                <small>Copyright {new Date().getFullYear()}</small>
+            </p>
+
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/firebase.init.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB9totyzBa_-tiGYn-osbtVARYkyGeRWKQ",
+    authDomain: "programming-fundamental-b1ee0.firebaseapp.com",
+    projectId: "programming-fundamental-b1ee0",
+    storageBucket: "programming-fundamental-b1ee0.appspot.com",
+    messagingSenderId: "621206857038",
+    appId: "1:621206857038:web:1c33d053842be5ab3bea1f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth;
