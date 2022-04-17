d9b135b4f3a9000ba9028337502fed8ef50668bd
ariful892 <ariful00892@gmail.com>
2022-04-17 15:51:05 +0600

    banner added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
+import './Banner.css';
 
-import banner1 from '../../../images/banner/banner1.jpg';
-import banner2 from '../../../images/banner/banner2.jpg';
-import banner3 from '../../../images/banner/banner3.jpg';
+import banner1 from '../../../images/banner/banner1.png';
+import banner2 from '../../../images/banner/banner2.png';
+import banner3 from '../../../images/banner/banner3.png';
+import banner4 from '../../../images/banner/banner4.png';
 
 const Banner = () => {
     return (
-        <div>
+        <div className='mt-3'>
+            <h1 className='text-center fs-1 mb-3'>Programming Fundamental</h1>
             <Carousel>
                 <Carousel.Item>
                     <img
@@ -42,6 +45,18 @@ const Banner = () => {
                     <Carousel.Caption>
                         <h3>Third slide label</h3>
                         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner4}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
