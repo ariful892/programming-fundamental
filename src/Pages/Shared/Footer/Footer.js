bec86dd160dc57773f157a7574787d79d4dabf9b
ariful892 <ariful00892@gmail.com>
2022-04-17 17:01:49 +0600

    homepage resonsive done

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopyright } from '@fortawesome/free-solid-svg-icons'
+
 import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className='text-center mt-5'>
+        <footer className='text-center mt-5 bg-dark text-white p-2'>
             <p>
-                <small>Copyright {new Date().getFullYear()}</small>
+                <small>Copyright <FontAwesomeIcon className='ms-2 me-2' icon={faCopyright}></FontAwesomeIcon> {new Date().getFullYear()}</small>
             </p>
 
         </footer>
