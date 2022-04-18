5c79f412e916b2cf897984170eb3ed148684d8e3
ariful892 <ariful00892@gmail.com>
2022-04-18 23:44:44 +0600

    deleted env

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId,
+    apiKey: "AIzaSyB9totyzBa_-tiGYn-osbtVARYkyGeRWKQ",
+    authDomain: "programming-fundamental-b1ee0.firebaseapp.com",
+    projectId: "programming-fundamental-b1ee0",
+    storageBucket: "programming-fundamental-b1ee0.appspot.com",
+    messagingSenderId: "621206857038",
+    appId: "1:621206857038:web:1c33d053842be5ab3bea1f"
 };
 
 // Initialize Firebase
